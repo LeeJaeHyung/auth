18d86e1615d6cd10b2e80652d70c9b880c5382cc
LeeJaeHyung <leejh7623@gmail.com>
2024-02-05 18:10:22 +0900

    pc-> 노트북

---
+++ src/main/java/com/sparta/springauth/entity/User.java
@@ -0,0 +1,37 @@
+package com.sparta.springauth.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users") //테이블 명이 users로 생성 됨
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)//enum타음을 데이터 베이스에 저장 User
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
